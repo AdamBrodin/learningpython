a0066feb5d9fc00f2ba3cc322dcab78cae0ac65c
AdamBrodin <worecraft@gmail.com>
2019-11-17 22:45:02 +0100

    Basic Strings

--- main.py
+++
@@ -1,4 +0,0 @@
-name = "Adam"
-age = 18
-print("My name is " + name)
-print('We\'re writing in Python')
---
+++ string.py
@@ -0,0 +1,5 @@
+firstname = "Adam"
+lastname = "Brodin"
+print("Hello, " + firstname)
+print("#1 lastname is " + lastname)
+print("Your full name is " + firstname + " " + lastname)
