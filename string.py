9c77562221ecec4a1fae22f1a6f8baa3bc110577
AdamBrodin <worecraft@gmail.com>
2019-11-20 14:48:56 +0100

    title()

--- string.py
+++ string.py
@@ -1,5 +1,7 @@
-firstname = "Adam"
-lastname = "Brodin"
+firstname = "adam"
+lastname = "brodin"
+
 print("Hello, " + firstname)
 print("#1 lastname is " + lastname)
-print("Your full name is " + firstname + " " + lastname)
+print("Your full name is " + firstname.title() + " " + lastname.title())
+# title() capitalises the first letter of the string
